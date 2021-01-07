27a952ca44ec3fa9dbc8042905a2edc3454b724e
bckim9489 <47053587+bckim9489@users.noreply.github.com>
2021-01-07 16:32:55 +0900

    ActionChain stded

--- seleniumTester.py
+++ seleniumTester.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from time import sleep
 
 driver = webdriver.Chrome(r'C:\\chromedriver.exe')
@@ -16,8 +17,17 @@ elm_id.send_keys('wkacadm')
 elm_pw.send_keys('1')
 btn_elm.click()
 
-elm_menu = driver.find_elements_by_css_selector("#gnb > ul > li:nth-child(1) > ul > li:nth-child(1) > a")
-elm_menu[0].click()
+#상황정보
+elm_menu = driver.find_element_by_css_selector("#gnb > ul > li:nth-child(1) > a")
+action_chain = webdriver.ActionChains(driver).move_to_element(elm_menu)
+action_chain.perform()
+sleep(1)
+driver.find_element_by_css_selector("#gnb > ul > li:nth-child(1) > ul > li:nth-child(1) > a").click()
+#   > 상황정보조회 (1. 셀렉트박스, 2. 캘린더, 3. 버튼,  4. 테이블 값, 5. 페이지네이션)
+#       > 검색 테스트 (1. 셀렉트박스1 -> 캘린더(분기설정) -> 셀렉트박스2 -> 샐렉트박스3 -> 검색버튼)
+
+
+
 
 
 
